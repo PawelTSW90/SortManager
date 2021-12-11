28b8531031e27addd0be2119020eaa25163cc7c6
Pawel <pawel.dyjak@poczta.onet.pl>
2021-12-11 13:28:09 +0000

    Bubble sorter algorithm refactored

--- src/main/java/com/sparta/paweldyjak/sorters/BubbleSorter.java
+++ src/main/java/com/sparta/paweldyjak/sorters/BubbleSorter.java
@@ -1,29 +1,33 @@
 package com.sparta.paweldyjak.sorters;
-
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class BubbleSorter {
+    private Long sortingTime;
 
-    public Integer[] bubbleSortNumber(Integer[] arrayToSort){
+    public int[] bubbleSortNumber(int[] array){
         long startTime = System.nanoTime();
         boolean noIteration = false;
-        ArrayList<Integer> numbersList = new ArrayList<>(Arrays.asList(arrayToSort));
-
+        int tmpNumber;
         //repeat iteration until there is no new iteration needed
         while (!noIteration) {
             noIteration = true;
-            for (int i = 1; i < numbersList.size(); i++) {
-                if (numbersList.get(i) < numbersList.get(i-1)) {
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i-1]) {
                     noIteration = false;
-                    Collections.swap(numbersList, i, i-1);
+                    tmpNumber = array[i-1];
+                    array[i-1] = array[i];
+                    array[i] = tmpNumber;
                     i -= 1;
                 }
             }
         }
         long endTime = System.nanoTime();
-        System.out.println("Time: "+(endTime - startTime)+" nanoseconds");
-        return numbersList.toArray(new Integer[0]);
+        sortingTime = endTime-startTime;
+        return array;
+    }
+
+    public static void main(String[] args) {
+        BubbleSorter bubbleSorter = new BubbleSorter();
+        System.out.println(Arrays.toString(bubbleSorter.bubbleSortNumber(new int[]{5,4,3,2,1})));
     }
 }
