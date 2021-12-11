ffacd145f7eb65adf6d3d53766d8a2ba2ff920b2
Pawel <pawel.dyjak@poczta.onet.pl>
2021-12-11 13:38:40 +0000

    Bubble sorter algorithm tested

--- src/main/java/com/sparta/paweldyjak/sorters/BubbleSorter.java
+++ src/main/java/com/sparta/paweldyjak/sorters/BubbleSorter.java
@@ -26,8 +26,4 @@ public class BubbleSorter {
         return array;
     }
 
-    public static void main(String[] args) {
-        BubbleSorter bubbleSorter = new BubbleSorter();
-        System.out.println(Arrays.toString(bubbleSorter.bubbleSortNumber(new int[]{5,4,3,2,1})));
-    }
 }
