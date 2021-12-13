f5df91ad76cefef1cdcc874d1706ad6abc32dfde
Pawel <pawel.dyjak@poczta.onet.pl>
2021-12-13 15:41:01 +0000

    Merge Sorter before testing

--- src/main/java/com/sparta/paweldyjak/sorters/MergeSorter.java
+++ src/main/java/com/sparta/paweldyjak/sorters/MergeSorter.java
@@ -50,7 +50,6 @@ public class MergeSorter implements Sorters {
             }
         }
     }
-
     public int[] mergeArray(int[] firstArray, int[] secondArray, int startIndexOfArraysToMerge) {
         int firstArrayLength = firstArray.length;
         int secondArrayLength = secondArray.length;
@@ -118,46 +117,3 @@ public class MergeSorter implements Sorters {
         return "Merge Sorter";
     }
 }
-/*    public int[] merge() {
-        int firstArrayIndex = 0;
-        int secondArrayIndex = 0;
-        int mergePosition = 0;
-        int[] mergedArray;
-        //merge arrays until unsortedArrayList is empty
-        while (unsortedArrayList.size() != 0) {
-            mergedArray = new int[unsortedArrayList.get(0).length * 2];
-            for (int i = 0; i < mergedArray.length; i++) {
-                if (unsortedArrayList.get(0)[firstArrayIndex] < unsortedArrayList.get(1)[secondArrayIndex]) {
-                    mergedArray[mergePosition] = unsortedArrayList.get(0)[firstArrayIndex];
-                    firstArrayIndex++;
-                } else {
-                    mergedArray[mergePosition] = unsortedArrayList.get(1)[secondArrayIndex];
-                    secondArrayIndex++;
-                }
-                mergePosition++;
-
-                if (firstArrayIndex == unsortedArrayList.get(0).length || secondArrayIndex == unsortedArrayList.get(1).length) {
-                    if (firstArrayIndex == unsortedArrayList.get(0).length) {
-                        for (int j = mergePosition; j < mergedArray.length; j++) {
-                            mergedArray[mergePosition] = unsortedArrayList.get(1)[secondArrayIndex];
-                            mergePosition++;
-                            secondArrayIndex++;
-                        }
-                    }
-                    if (secondArrayIndex == unsortedArrayList.get(1).length) {
-                        for (int j = mergePosition; j < mergedArray.length; j++) {
-                            mergedArray[mergePosition] = unsortedArrayList.get(0)[firstArrayIndex];
-                            mergePosition++;
-                            firstArrayIndex++;
-                        }
-                    }
-                    unsortedArrayList.remove(0);
-                    unsortedArrayList.remove(0);
-                    unsortedArrayList.add(mergedArray);
-                    merge();
-                }
-            }
-        }
-        return new int[1];
-
-    }*/
