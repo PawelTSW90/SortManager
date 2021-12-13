9b65ee98ba6e85844e80441f58c6e1d9a4e7ff5a
Pawel <pawel.dyjak@poczta.onet.pl>
2021-12-13 15:32:43 +0000

    Merge Sorter before testing

--- src/main/java/com/sparta/paweldyjak/sorters/MergeSorter.java
+++ src/main/java/com/sparta/paweldyjak/sorters/MergeSorter.java
@@ -2,48 +2,110 @@ package com.sparta.paweldyjak.sorters;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class MergeSorter implements Sorters {
-    List<int[]> unsortedArrayList = new ArrayList<>();
-    private int currentIndex;
+    private final List<int[]> arrayListToSort = new ArrayList<>();
 
-
-    @Override
-    public int[] sort(int[] arrayToSort) {
-
-
-        return new int[0];
-    }
-    public void splitArray(int[] arrayToSplit) {
-        int arrayMiddle = arrayToSplit.length / 2;
-        int[] firstArray = new int[arrayMiddle];
-        int[] secondArray = new int[arrayToSplit.length - arrayMiddle];
-        for (int i = 0; i <firstArray.length; i++) {
-            firstArray[i] = arrayToSplit[i];
-
-        }
-        for (int i = 0; i <secondArray.length; i++) {
-            secondArray[i] = arrayToSplit[arrayMiddle+i];
-        }
-        if(firstArray.length==1){
-            unsortedArrayList.add(firstArray);
-        } else{
-            splitArray(firstArray);
-        }
-        if(secondArray.length==1){
-            unsortedArrayList.add(secondArray);
-        } else{
-            splitArray(secondArray);
-        }
-    }
 
     public static void main(String[] args) {
         MergeSorter mergeSorter = new MergeSorter();
-        mergeSorter.splitArray(new int[]{1,2,3,4,5,6,7,8,9});
+        System.out.println(Arrays.toString(mergeSorter.sort(new int[]{1})));
+
+
     }
 
+    @Override
+    public int[] sort(int[] arrayToSort) {
+        if (arrayToSort.length == 1) {
+            return arrayToSort;
+        } else {
+            splitArray(arrayToSort);
+            return mergeArray(arrayListToSort.get(0), arrayListToSort.get(1), 0);
+        }
+    }
+
+    public void splitArray(int[] arrayToSplit) {
+        if (arrayToSplit.length == 1) {
+            arrayListToSort.add(arrayToSplit);
+        } else {
+            int arrayMiddle = arrayToSplit.length / 2;
+
+            //split array into two arrays and copy its values
+            int[] firstArray = new int[arrayMiddle];
+            int[] secondArray = new int[arrayToSplit.length - arrayMiddle];
+            System.arraycopy(arrayToSplit, 0, firstArray, 0, firstArray.length);
+            System.arraycopy(arrayToSplit, arrayMiddle, secondArray, 0, secondArray.length);
+
+            //do method recursion until array is split into single elements arrays
+            if (firstArray.length == 1) {
+                arrayListToSort.add(firstArray);
+            } else {
+                splitArray(firstArray);
+            }
+            if (secondArray.length == 1) {
+                arrayListToSort.add(secondArray);
+            } else {
+                splitArray(secondArray);
+            }
+        }
+    }
+
+    public int[] mergeArray(int[] firstArray, int[] secondArray, int startIndexOfArraysToMerge) {
+        int firstArrayLength = firstArray.length;
+        int secondArrayLength = secondArray.length;
+        int firstArrayIndex = 0, secondArrayIndex = 0, mergeIndex = 0;
+        //prepare new array for two arrays merged
+        int[] mergedArray = new int[firstArrayLength + secondArrayLength];
+
+        /*if current value of first array is larger than current value of second array,
+        add it to mergedArray. If not, add current value of second array*/
+        for (int i = 0; i < mergedArray.length - 1; i++) {
+            if (firstArray[firstArrayIndex] < secondArray[secondArrayIndex]) {
+                mergedArray[mergeIndex] = firstArray[firstArrayIndex];
+                firstArrayIndex++;
+            } else {
+                mergedArray[mergeIndex] = secondArray[secondArrayIndex];
+                secondArrayIndex++;
+            }
+            mergeIndex++;
+
+            /*if all values of one of the array has been added to mergedArray,
+            move to mergedArray remaining values of other array */
+            if (firstArrayIndex == firstArrayLength) {
+                for (int j = mergeIndex; j < mergedArray.length; j++) {
+                    mergedArray[mergeIndex] = secondArray[secondArrayIndex];
+                    secondArrayIndex++;
+                    mergeIndex++;
+                    i++;
+                }
+                continue;
+            }
+            if (secondArrayIndex == secondArrayLength) {
+                for (int j = mergeIndex; j < mergedArray.length; j++) {
+                    mergedArray[mergeIndex] = firstArray[firstArrayIndex];
+                    firstArrayIndex++;
+                    mergeIndex++;
+                    i++;
+                }
+            }
+        }
+        //add mergedArray to arrayListToSort and remove arrays that has been merged
+        arrayListToSort.set(startIndexOfArraysToMerge, mergedArray);
+        arrayListToSort.remove(startIndexOfArraysToMerge + 1);
+
+        //if whole arrayList has been iterated or there is odd number of remaining arrays,
+        // start iteration from beginning of the arrayList by setting indexOfArraysToMerge to 0
+        if (arrayListToSort.size() > 1) {
+            if (startIndexOfArraysToMerge == arrayListToSort.size() - 2 || startIndexOfArraysToMerge == arrayListToSort.size() - 1) {
+                startIndexOfArraysToMerge = 0;
+            } else {
+                startIndexOfArraysToMerge++;
+            }
+            mergeArray(arrayListToSort.get(startIndexOfArraysToMerge), arrayListToSort.get(startIndexOfArraysToMerge + 1), startIndexOfArraysToMerge);
+        }
+        return arrayListToSort.get(0);
+    }
 
 
     @Override
