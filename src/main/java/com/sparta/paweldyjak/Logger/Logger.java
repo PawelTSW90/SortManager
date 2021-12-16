09b4bd0cf34c24d69ba3eb86b6fb328df0238915
Pawel <pawel.dyjak@poczta.onet.pl>
2021-12-16 20:56:56 +0000

    adding Logger messages

--- src/main/java/com/sparta/paweldyjak/Logger/Logger.java
+++ src/main/java/com/sparta/paweldyjak/Logger/Logger.java
@@ -3,17 +3,18 @@ package com.sparta.paweldyjak.Logger;
 import java.io.IOException;
 import java.util.logging.*;
 
-public class MyLogger {
-    private static final Logger logger = java.util.logging.Logger.getLogger("Sorters Logger");
+public class Logger {
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger("Sorters Logger");
     public static void log(Level level, String message){
         logger.log(level, message);
     }
     public static void configLogger(){
         try {
             setLoggerLevel(Level.ALL);
-            Handler fileHandler = new FileHandler("src/main/java/com/sparta/paweldyjak/myLog.log", true);
+            Handler fileHandler = new FileHandler("src/main/java/com/sparta/paweldyjak/sortManagerLog.log", true);
             logger.addHandler(fileHandler);
             fileHandler.setFormatter(new LoggerFormatter());
+            Logger.log(Level.FINE, "Logger configuration finished");
         } catch (IOException e) {
             e.printStackTrace();
         }
