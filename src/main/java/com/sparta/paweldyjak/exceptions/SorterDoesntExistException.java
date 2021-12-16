2a1c1ea650c7d251d992c9124fe4c309a9d9c905
Pawel <pawel.dyjak@poczta.onet.pl>
2021-12-16 15:43:44 +0000

    Check if sorter exists in file method added

---
+++ src/main/java/com/sparta/paweldyjak/exceptions/SorterDoesntExistException.java
@@ -0,0 +1,9 @@
+package com.sparta.paweldyjak.exceptions;
+
+public class SorterDoesntExistException extends Exception{
+
+
+    public SorterDoesntExistException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/sparta/paweldyjak/exceptions/WrongUserInputEsception.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.paweldyjak.exceptions;
-
-public class WrongUserInputEsception extends Exception{
-
-    public WrongUserInputEsception(String message){
-        super(message);
-    }
-
-}
