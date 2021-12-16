3d15eca8c4a28d1a4cc3317be8a6789c3adaa431
Pawel <pawel.dyjak@poczta.onet.pl>
2021-12-16 21:02:47 +0000

    Logger messages added

--- src/main/java/com/sparta/paweldyjak/SortManagerLoader.java
+++ src/main/java/com/sparta/paweldyjak/SortManagerLoader.java
@@ -5,6 +5,7 @@ import com.sparta.paweldyjak.display.OutputPrinter;
 import com.sparta.paweldyjak.display.UserInputScanner;
 import com.sparta.paweldyjak.sorters.Sorters;
 import com.sparta.paweldyjak.sorters.SortersFactory;
+
 import java.io.*;
 import java.util.Arrays;
 import java.util.logging.Level;
@@ -30,19 +31,22 @@ public class SortManagerLoader {
 
         //set sorter chosen by user
         Sorters sorter = SortersFactory.getSorter(sorterNumber);
+        Logger.log(Level.FINE, "Sorter object created");
 
         //run sorter and get its return values
         int[] sortedArray = sorter.sort(Arrays.copyOf(unsortedArray, unsortedArray.length));
+        Logger.log(Level.FINE, "Sorting finished");
         Long sortingTime = sorter.getSortingTime();
         String sorterName = sorter.getSorterName();
 
         //call method that shows final output
         OutputPrinter.printMessage(OutputPrinter.generateFinalMessage(sorterName, unsortedArray, sortedArray, sortingTime));
+        Logger.log(Level.FINE, "Final message printed. Closing application");
 
     }
 
     public static boolean checkIfSorterClassExists(int sorterNumber) {
-        if(sorterNumber<1){
+        if (sorterNumber < 1) {
             return false;
         }
         int lineNumber = 1;
